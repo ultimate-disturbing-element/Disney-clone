3e55368c69cc0c520ac56ccd4557cbb9ad8f6267
Abhinav <A15930@aakashleap.com>
2023-11-04 13:32:40 +0530

    Added Firebase KEYs

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import firebase from 'firebase';
-
+import dotenv from 'dotenv';
+dotenv.config();
 
 const firebaseConfig = {
-     apiKey: "AIzaSyBre1omtRhnuU2zUhD14cMNkjHIV0KYgFw",
-     authDomain: "disneyclonereact.firebaseapp.com",
-     projectId: "disneyclonereact",
-     storageBucket: "disneyclonereact.appspot.com",
-     messagingSenderId: "730759567375",
-     appId: "1:730759567375:web:737beac0c86549a529ab7a",
-     measurementId: "G-SV5XSY4TGK"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId:process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASURMENT_ID
   };
   // Initialize Firebase
   const firebaseApp = firebase.initializeApp(firebaseConfig);
