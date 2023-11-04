ad72f6a158b9da036e57ab94a5e34bad2e2283a6
Abhinav <A15930@aakashleap.com>
2023-11-04 15:19:24 +0530

    Security Enhanced

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,13 @@
 import firebase from 'firebase';
-import dotenv from 'dotenv';
-dotenv.config();
-
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId:process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASURMENT_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASURMENT_ID
   };
   // Initialize Firebase
   const firebaseApp = firebase.initializeApp(firebaseConfig);
